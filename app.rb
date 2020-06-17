fd64a71c1bcf4259faa681919e75dd6a7be435a4
claudia v <claudia4ro@yahoo.com>
2020-06-17 19:18:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end 
+  
+  post '/piglatinize' do 
+    text_from_user = params[:user_text]
+    @output_text = PigLatinizer.new.latinizer(text_from_user)
+    @output_text
+  end 
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer 
+  
+  attr_accessor :text 
+  
+ 
+  
+  def latinizer(text)
+    all_letters = ("a".."z").to_a
+    vowels = ["a", "e", "i", "o", "u"]
+    consonants = all_letters - vowels
+    arr_latinized = []
+    text.split.each do |word|
+      if vowels.include?(word[0])
+        arr_latinized << word + "way"
+      elsif consonants.include?(word[0]) && consonants.include?(word[1])
+        arr_latinized <<  word[2..-1] + word[0] + word[1] + "ay"
+      elsif consonants.include?(word[0])
+        arr_latinized << word[1..-1] + word[0] + "ay"
+      
+      end 
+    end
+    arr_latinized.join(" ")
+    
+  end 
+  
+  
+end 
+  
